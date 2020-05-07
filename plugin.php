18cba36c6832bd19a38458f47ba8daf1f08d4485
Egor <Egor3f@users.noreply.github.com>
2020-05-07 07:08:10 +0300

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -9,13 +9,12 @@ Author: Egor Aristov
 Author URI: https://efprojects.com
 */
 
-$chars = '/.';
 
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
 yourls_add_filter( 'get_shorturl_charset', 'slash_in_charset' );
 function slash_in_charset( $in ) {
-        return $in.$chars;
+        return $in.'/.';
 }
 
 
